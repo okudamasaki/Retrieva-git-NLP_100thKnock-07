1df65a3f4e01320a0411efe1f1cf838275d9ca9d
okudamasaki <okudamasaki@gmail.com>
2017-05-05 22:02:46 +0900

    初めてのコミット

---
+++ 07.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+x = 12
+y = u'気温'
+z = 22.4
+
+def function(x, y, z):
+	return unicode(x) + u'時の' + unicode(y) + u'は' + unicode(z)
+
+print(function(x, y, z))
